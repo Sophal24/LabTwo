4256547d41a14552937fbf49972db7028e2049fd
Taingon <iamtaingon@gmail.com>
2019-05-23 11:45:35 +0700

    Update

--- ex1.php
+++ ex1.php
@@ -1,26 +1,23 @@
 <?php
 	echo "Exercise 1";
-
-	// Find min max number in array
-	
 	$number = array(3 , -4 , 32 , -7 , 10 , -23 , 9);
-		$max = $number[0];
-		$min = $number[0];
+	$max = $number[0];
+	$min = $number[0];
 
 
-		for ($i=0; $i < 10 ; $i++) { 
+	for ($i=0; $i < 10 ; $i++) { 
 			
-			if ($max < $number[$i]) {
-				$max = $number[$i];
-			}
-
-			if ($min > $number[$i]) {
-				$min = $number[$i];
-			}
+		if ($max < $number[$i]) {
+			$max = $number[$i];
 		}
 
-		echo "Max Number : ".$max;
-		echo "<br>";
-		echo "Min Number : ".$min;
+		if ($min > $number[$i]) {
+			$min = $number[$i];
+		}
+	}
+
+	echo "Max Number : ".$max;
+	echo "<br>";
+	echo "Min Number : ".$min;
 
 ?>
