4f0beda2dcf01609bf8373a63e72b37657900c7c
Sophal24 <sophalisco23@gmail.com>
2019-05-28 11:43:36 +0700

    update function and recursion

--- function_ex1.php
+++ function_ex1.php
@@ -2,5 +2,30 @@
 
 	echo "Exercise 1 of FUNCION and RECURSION";
 	echo "<br>";
+
+	
+
+	// Set to default time zone (for Cambodia) - able to show same time to COMPUTER
+	date_default_timezone_set('Asia/Bangkok');
+	$date = date("d/m/y");
+	$time1 = date("h:i:s a"); 
+	
+	echo "Date : ".$date."<br>";
+	echo "Time : ".$time1."<br>";
+
+	// 24-hour format of an hour without leading zeros (0 through 23)
+	$Hour = date('G');
+	echo $Hour;
+
+	if ( $Hour >= 5 && $Hour <= 11 ) {
+	    
+	    echo "<h1 style='text-align: center;color: blue;'>Good Morning</h1>";
+	} else if ( $Hour >= 12 && $Hour <= 18 ) {
+	    
+	    echo "<h1 style='text-align: center;color: blue;'>Good Afternoon</h1>";
+	} else if ( $Hour >= 19 || $Hour <= 4 ) {
+	    
+	    echo "<h1 style='text-align: center;color: blue;'>Good Evening</h1>";
+	}
 	
 ?>
