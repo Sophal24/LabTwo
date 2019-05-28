3cdad0df83591b469b93f1b006a4871d5d376081
Sophal24 <sophalisco23@gmail.com>
2019-05-28 10:57:34 +0700

    update

--- ex5.php
+++ ex5.php
@@ -1,12 +1,18 @@
 <?php
 	echo "Exercise 5";
 	echo "<br>";
-	$a = array("a","b","c","d","e","f","g","h","i");
-	$b = array("j","k","l","m","n","p","x","y","z");
-	$c = array();
-	for ($i=0; $i <sizeof($a) ; $i++) { 
-		$c[$i] = $a[$i]."+".$b[$i];
-		echo "$c[$i]";
+
+	$a = array(array("a","b","c"),array("d","e","f"),array("g","h","i"));
+	$b = array(array("j","k","l"),array("m","n","p"),array("x","y","z"));
+	$c = array(array(),array(),array());
+	
+
+	for ($i=0; $i < sizeof($a); $i++) { 
+		for ($j=0; $j < sizeof($b); $j++) { 
+			# code...
+			$c[$i][$j] = $a[$i][$j]." + ".$b[$i][$j]." ";
+			echo $c[$i][$j];
+		}
 		echo "<br>";
-	}	
+	}
 ?>
